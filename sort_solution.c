d23840c5f5c4199e74a694d18fe7fb08a0cccf41
orizhiy <sklif.85@gmail.com>
2017-02-26 18:00:07 +0200

    qsort_list_4

--- sort_solution.c
+++ sort_solution.c
@@ -14,86 +14,21 @@ void ft_end(t_stack **end, t_stack *head)
 
 void ft_swap_list(t_stack **i, t_stack **j)
 {
-    t_stack *tail;
-    t_stack *next;
 
-    if (*i != *j)
-    {
-        next = (*j)->next;
-        tail = (*i)->prev;
-        (*i)->next = next;
-        (*j)->prev = tail;
-        (*i)->prev = *j;
-        (*j)->next = *i;
-        tail ? tail->next = *j : 0;
-
-        //ft_swap(i);
-        *i = *j;
-        *j = (*j)->next;
-    }
 }
 
 t_stack *ft_partition_stack(t_stack **a, t_stack **start, t_stack **end)
 {
-    t_stack *i;
-    t_stack *j;
-    t_stack *pivot;
 
-    //j = *start;
-    //i = j != *end ? *start : (*start)->prev;
-    i = (*start)->prev;
-    j = *start;
-    pivot = *end;
-
-    //while (j != *end)
-    while (j && j != *end)
-    {
-        if (j->val <= pivot->val)
-        {
-            if (!i)
-                i = *start;
-            else
-                i = i->next;
-            ft_swap_list(&i, &j);
-        }
-        j = j->next;
-    }
-    if (!i)
-    {
-        ft_swap_list(start, end);
-        return ((*start)->next);
-    }
-    else
-    {
-        ft_swap_list(&(i->next), end);
-        return (i->next);
-    }
 }
 void ft_qsort_stack(t_stack **a, t_stack **b, t_stack *start, t_stack *end)
 {
-    t_stack *p;
 
-    if (start != end)
-    {
-        p = ft_partition_stack(a, &start, &end);
-        ft_qsort_stack(a, b, start, p->prev);
-        ft_qsort_stack(a, b, p->next, end);
-    }
 }
 
 void ft_sort_sotution(t_stack *a, t_stack *b)
 {
-    t_stack *start;
-    t_stack *end;
 
-    start = a;
-    ft_end(&end, a);
-
-	ft_print_stack(a, b);
-
-    ft_qsort_stack(&a, &b, start, end);
-
-    ft_print_stack(a, b);
 
 }
 
